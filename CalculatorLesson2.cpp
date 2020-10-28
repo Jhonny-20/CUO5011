68faae187ecf5952d13cfe1f3a273975b573642f
Jhonny <jalbo1994_a16@hotmail.com>
2020-10-28 10:53:27 +0000

    Calculator Final

--- CalculatorLesson2.cpp
+++ CalculatorLesson2.cpp
@@ -5,43 +5,107 @@
 using namespace std;
 
 
+
 int main()
 {
+    int choice;
+    long num1, num2, x;
+ 
+    cout << "Please choose your option:"
+        "\n1 = Addition"
+        "\n2 = Subtraction"
+        "\n3 = Multiplication"
+        "\n4 = Division"
+        "\n5 = Squares"
+        "\n6 = power"
+        "\n7= Remainder: ";
+        "\n8= exit";
+
+    cin >> choice;
    
-    char op;
-    float num1, num2;
-
-    cout << "Enter operator either + or - or * or /: ";
-    cin >> op;
-
-    cout << "Enter two operands: ";
-    cin >> num1 >> num2;
-
-    switch (op)
+    while (choice < 1 || choice>8)
     {
-    case '+':
-        cout << num1 + num2;
-        break;
-
-    case '-':
-        cout << num1 - num2;
-        break;
-
-    case '*':
-        cout << num1 * num2;
-        break;
-
-    case '/':
-        cout << num1 / num2;
-        break;
-
-    default:
-        // If the operator is other than +, -, * or /, error message is shown
-        cout << "Error! operator is not correct";
-        break;
+        cout << "\nPlease choose one option"
+            "\nChoice: ";
+        cin >> choice;
     }
 
-    return 0;
+    switch (choice)
+    {
+        //Addition
+    case 1:
+        cout << "Enter two numbers: \n";
+        cin >> num1 >> num2;
+        x = num1 + num2;
+        cout << "Sum = " << x;
+        break;
+
+        //Subtraction
+    case 2:
+        cout << "Enter two numbers: \n";
+        cin >> num1 >> num2;
+        x = num1 - num2;
+        cout << "Subtraction = " << x;
+        break;
+
+        //Multiplication
+    case 3:
+        cout << "Enter two numbers: \n";
+        cin >> num1 >> num2;
+        x = num1 * num2;
+        cout << "Product = " << x;
+        break;
+
+        //Division
+    case 4:
+        cout << "Enter Dividend: ";
+        cin >> num1;
+        cout << "Enter Divisor: ";
+        cin >> num2;
+
+       //while loops tp check that the divisor is greater than zero
+        while (num2 >=1)
+        {
+            cout << "\nDivisor cannot be zero."
+                "\nEnter divisor once again: ";
+            cin >> num2;
+        }
+        x = num1 / num2;
+        cout << "\nQuotient = " << x;
+        break;
+
+        //Square
+    case 5:
+        cout << "Enter any number: \n";
+        cin >> num1;
+        x = num1 * num1;
+        cout << "Square = " << x;
+        break;
+
+        //power
+    case 6:
+        cout << "Enter base number: \n";
+        cin >> num1;
+        cout<<"enter exponent";
+        cin >> num2;
+        x = num1 * (num2*num2);
+        cout << "Square = " << x;
+        break;
+
+        //Remainder
+    case 7:
+        cout << "Enter two number to calculate the remainder: \n";
+        cin >> num1;
+        cin >> num2;
+        x = num1 % num2;
+        cout << "Remainder is  = " << x;
+        break;
+
+    case 8:
+        return 0;
+
+    default: cout << "\nError";
+    }
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
